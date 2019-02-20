e5895979984144fd8f21f1c7a719f55d6ecebd47
KieshaJ <duel.kj@gmail.com>
2019-02-20 13:09:51 +0200

    Carousel buttons and dots added, functionality tweaked

--- js/carousel/carousel.js
+++ js/carousel/carousel.js
@@ -1,14 +1,78 @@
-var myIndex = 0;
+var index = 0;
+var i;
+var slides = document.getElementsByClassName("slides");
+var dots = document.getElementsByClassName("dot");
+var timeout;
+
 carousel();
 
+function next() {
+	for (i = 0; i < slides.length; i++) {
+    	slides[i].style.display = "none";
+		dots[i].className = dots[i].className.replace(" active", "");
+  	}
+
+	index++;
+
+	if (index > slides.length) {
+		index = 1;
+	}
+
+	slides[index - 1].style.display = "block";
+	dots[index - 1].className += " active";
+
+	clearTimeout(timeout);
+	timeout = setTimeout(carousel, 5000);
+}
+
+function previous() {
+	for (i = 0; i < slides.length; i++) {
+    	slides[i].style.display = "none";
+		dots[i].className = dots[i].className.replace(" active", "");
+  	}
+
+	index--;
+
+	if (index < 1) {
+		index = slides.length;
+	}
+
+	slides[index - 1].style.display = "block";
+	dots[index - 1].className += " active";
+
+	clearTimeout(timeout);
+	timeout = setTimeout(carousel, 5000);
+}
+
+function pickSlide(n) {
+	for (i = 0; i < slides.length; i++) {
+    	slides[i].style.display = "none";
+		dots[i].className = dots[i].className.replace(" active", "");
+  	}
+
+	index = n;
+
+	slides[index - 1].style.display = "block";
+	dots[index - 1].className += " active";
+
+	clearTimeout(timeout);
+	timeout = setTimeout(carousel, 5000);
+}
+
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}
-  x[myIndex-1].style.display = "block";
-  setTimeout(carousel, 3000); // Change image every 2 seconds
+  	for (i = 0; i < slides.length; i++) {
+    	slides[i].style.display = "none";
+		dots[i].className = dots[i].className.replace(" active", "");
+  	}
+
+  	index++;
+
+  	if (index > slides.length) {
+		index = 1;
+	}
+
+  	slides[index - 1].style.display = "block";
+	dots[index - 1].className += " active";
+
+	timeout = setTimeout(carousel, 5000);
 }
